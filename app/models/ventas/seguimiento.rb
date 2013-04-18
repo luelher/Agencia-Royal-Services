72571f014839d20f48eba97e2c5bb05ede43d3c2
root <root@server02.agenciaroyal>
2013-04-18 17:41:22 -0400

    Se agregaron otros motivos

--- app/models/ventas/seguimiento.rb
+++ app/models/ventas/seguimiento.rb
@@ -1,5 +1,17 @@
 class Ventas::Seguimiento < ActiveRecord::Base
   self.table_name = 'seguimientos'
+  MOTIVOS = [
+	['Llamada Telefonica'],
+	['Llamada Telefonica de Cliente'],
+	['SMS Enviado'],
+	['Paso por Oficina'],
+	['Correo Electronico'],
+	['Carta Enviada'],
+	['Carta Ultimo Aviso'],
+	['Carta Legal'],
+	['Correo Electronico'],
+	['Correo Electronico']
+        ]
   attr_accessible :cliente_id, :motivo, :observacion, :usuario, :plazo_pago, :factura, :user_id
 
   belongs_to :cliente, :class_name => "Ventas::Cliente"
